cd5495b348e6ebc345a591798f8c6c107c31f529
Yesudeep Mangalapilly <yesudeep@google.com>
2012-03-20 10:44:22 +0530

    Compile all elisp modules when init.el is saved.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@google.com>

--- init.el
+++ init.el
@@ -131,17 +131,18 @@
     (when (file-exists-p (concat user-init-file ".elc"))
       (delete-file (concat user-init-file ".elc")))
     (byte-compile-file user-init-file)
+    (byte-compile-dotfiles)
     ;; (message "%s compiled" user-init-file)
     ))
 
 (defun my-emacs-lisp-mode-hook ()
   (when (equal buffer-file-name user-init-file)
-    (add-hook 'after-save-hook 'byte-compile-user-init-file t t)
-    (add-hook 'after-save-hook 'byte-compile-dotfiles)))
+    (add-hook 'after-save-hook 'byte-compile-user-init-file t t)))
 
 ;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
 (add-hook 'kill-emacs-hook 'byte-compile-user-init-file t t)
+
 
 ;; Automatically compile all modules on startup.
 ;; Don't enable this because it takes too much time
