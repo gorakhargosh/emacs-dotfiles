d09cf8724395a7798eaea179fb7268f666f32178
Yesudeep Mangalapilly <yesudeep@google.com>
2012-03-27 23:14:58 +0530

    Menu bar mode is on in init.el
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@google.com>

--- mia.el
+++ mia.el
@@ -1,7 +1,7 @@
 ;;; mia.el --- Configuration specific to Mia.
 
-(menu-bar-mode t)
 ;(set-face-attribute 'default nil :family "Monaco" :height 140)
 (set-face-attribute 'default nil :family "Consolas" :height 160)
+
 
 ;;; mia.el ends here.
