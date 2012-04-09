f753acadecabf8f6c521a918e3d4409ebdb14915
Yesudeep Mangalapilly <yesudeep@google.com>
2012-04-09 18:22:15 +0530

    Disables running pylint on save. Buggy.

--- init.el
+++ init.el
@@ -357,10 +357,10 @@
 (autoload 'python-pylint "python-pylint")
 (autoload 'pylint "python-pylint")
 
-(defun onsave-run-pylint ()
-  "Runs pylint on the code as soon as the Python file is saved."
-  (add-hook 'after-save-hook 'python-pylint))
-(add-hook 'python-mode-hook 'onsave-run-pylint)
+;; (defun onsave-run-pylint ()
+;;   "Runs pylint on the code as soon as the Python file is saved."
+;;   (add-hook 'after-save-hook 'python-pylint))
+;; (add-hook 'python-mode-hook 'onsave-run-pylint)
 
 ;; Autocompletion using pysmell.
 ;; (require 'pysmell)
