eb38344ecdaef9bae81a24c5dbcd7790a825d693
Yesudeep Mangalapilly <yesudeep@google.com>
2012-06-06 10:14:22 +0530

    Change font for mia.

--- mia.el
+++ mia.el
@@ -1,7 +1,7 @@
 ;;; mia.el --- Configuration specific to Mia.
 
-(set-face-attribute 'default nil :family "Monaco" :height 140)
-;;(set-face-attribute 'default nil :family "Consolas" :height 160)
+;;(set-face-attribute 'default nil :family "Monaco" :height 140)
+(set-face-attribute 'default nil :family "Consolas" :height 150)
 
 
 
